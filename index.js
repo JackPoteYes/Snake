ba75585a1ddd02e874bfa306ac5f39a763fc7a10
JackPote <guilhem.forey@hotmail.fr>
2019-08-24 21:13:01 +0100

    Can pilot single head

--- index.js
+++ index.js
@@ -2,54 +2,56 @@ const shadesNb = 5;
 let count = 0;
 var loopPeriod = 75;
 let cells = document.getElementsByClassName("cell-content");
-toggleCell(cells[0])
+let direction = "right";
+let headIndex = 0;
+toggleCell(cells[headIndex]);
 
-let cellsObjects = [];
-for (let i = 0; i < cells.length; i ++)
-{
-    cellsObjects.push(
-        {
-            element: cells[i],
-            state: i % shadesNb,
-        }
-    );
+var mainLoop = setInterval(() => {
+  let newIndex = 0;
+  switch (direction) {
+    case "right":
+      newIndex = headIndex % 12 === 11 ? headIndex - 11 : headIndex + 1;
+      break;
+    case "left":
+      newIndex = headIndex % 12 === 0 ? headIndex + 11 : headIndex - 1;
+      break;
+    case "down":
+      newIndex = (headIndex + 12) % cells.length;
+      break;
+    case "up":
+      newIndex =
+        headIndex < 12 ? cells.length - 12 + headIndex : headIndex - 12;
+      break;
+  }
+  updateHead(newIndex);
+}, 100);
+
+document.addEventListener("keydown", event => {
+  switch (event.keyCode) {
+    case 40:
+      direction = "down";
+      break;
+    case 37:
+      direction = "left";
+      break;
+    case 38:
+      direction = "up";
+      break;
+    case 39:
+      direction = "right";
+      break;
+  }
+});
+
+function updateHead(newIndex) {
+  cells[headIndex].className = "cell-content";
+  cells[newIndex].className = "cell-content snake";
+  headIndex = newIndex;
 }
-
-
-
-var mainLoop = createMainLoopInterval(loopPeriod);
-
-
-function changeLoopSpeed(increase)
-{
-    clearInterval(mainLoop);
-    loopPeriod += increase ? -10 : 10;
-    mainLoop = createMainLoopInterval(loopPeriod);
-}
-
-function createMainLoopInterval(loopPeriod)
-{
-    return setInterval(() => {
-        for (let i = 0; i < cellsObjects.length; i++)
-        {
-            cellsObjects[i].state ++;
-            cellsObjects[i].element.className = "cell-content selected-" + (cellsObjects[i].state % shadesNb).toString();
-        }
-    }, loopPeriod);
-}
-
 
 function toggleCell(cellElement) {
-    cellElement.className = (cellElement.className.length > "cell-content".length ? "cell-content" : "cell-content selected");
+  cellElement.className =
+    cellElement.className.length > "cell-content".length
+      ? "cell-content"
+      : "cell-content snake";
 }
-
-document.addEventListener("keydown", (event) => {
-    if (event.keyCode === 40)
-    {
-        changeLoopSpeed(false);
-    }
-    else if(event.keyCode === 38)
-    {
-        changeLoopSpeed(true);
-    }
-})
