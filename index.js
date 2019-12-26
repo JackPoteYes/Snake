5a0960bf7b30e265679e392c5727698cc89712b0
JackPote <guilhem.forey@hotmail.fr>
2019-12-26 09:03:14 +0000

    First snake

--- index.js
+++ index.js
@@ -2,54 +2,56 @@ const CELLS = [];
 const GRID_LENGTH = 12;
 const ROOT_ID = "root";
 const SHADES_NB = 5;
-const INITIAL_HEAD_INDEX = [0, 0];
+const INITIAL_HEAD_INDEX = [0, 1];
 const LOOP_PERIOD = 100;
 
 let direction = "right";
 let count = 0;
-let headIndex = INITIAL_HEAD_INDEX;
+let snake = [INITIAL_HEAD_INDEX, [0, 0]];
 
-let MAIN_LOOP = null;
+var MAIN_LOOP = null;
+var RUNNING = false;
 
-initPage(() => {
-  loop();
-});
+initPage();
 
 function loop() {
   MAIN_LOOP = setInterval(() => {
-    let newIndex = [null, null];
+    const headIndex = snake[0];
+    let newHeadIndex = [null, null];
     switch (direction) {
       case "right":
-        newIndex =
+        newHeadIndex =
           headIndex[1] % 12 === 11
             ? [headIndex[0], headIndex[1] - 11]
             : [headIndex[0], headIndex[1] + 1];
         break;
       case "left":
-        newIndex =
+        newHeadIndex =
           headIndex[1] % 12 === 0
             ? [headIndex[0], headIndex[1] + 11]
             : [headIndex[0], headIndex[1] - 1];
         break;
       case "down":
-        newIndex =
+        newHeadIndex =
           headIndex[0] % 12 === 11
             ? [headIndex[0] - 11, headIndex[1]]
             : [headIndex[0] + 1, headIndex[1]];
         break;
       case "up":
-        newIndex =
+        newHeadIndex =
           headIndex[0] % 12 === 0
             ? [headIndex[0] + 11, headIndex[1]]
             : [headIndex[0] - 1, headIndex[1]];
         break;
     }
-    updateHead(newIndex);
+    moveSnake(newHeadIndex);
   }, LOOP_PERIOD);
+  RUNNING = true;
 }
 
 function stopLoop() {
   clearInterval(MAIN_LOOP);
+  RUNNING = false;
 }
 
 document.addEventListener("keydown", event => {
@@ -66,13 +68,24 @@ document.addEventListener("keydown", event => {
     case 39:
       direction = "right";
       break;
+    case 32:
+      RUNNING ? stopLoop() : loop();
+      break;
   }
 });
 
-function updateHead(newIndex) {
-  toggleCell(getCell(...headIndex));
-  toggleCell(getCell(...newIndex));
-  headIndex = newIndex;
+function moveSnake(newIndex) {
+  // Empty last bit
+  emptyCell(getCell(...snake[snake.length - 1]));
+
+  // Update snake's indexes
+  for (let i = 0; i < snake.length - 1; i++) {
+    snake[i + 1] = snake[i];
+  }
+  snake[0] = newIndex;
+
+  // Fill first bit (head)
+  fillCell(getCell(...snake[0]));
 }
 
 function toggleCell(cellElement) {
@@ -82,11 +95,19 @@ function toggleCell(cellElement) {
       : "cell-content snake";
 }
 
+function fillCell(cellElement) {
+  cellElement.className = "cell-content snake";
+}
+
+function emptyCell(cellElement) {
+  cellElement.className = "cell-content";
+}
+
 function getCell(x, y) {
   return CELLS[x][y];
 }
 
-function initPage(cb) {
+function initPage() {
   const container = document.getElementById(ROOT_ID);
   const table = document.createElement("table");
   for (let row = 0; row < GRID_LENGTH; row++) {
@@ -105,6 +126,5 @@ function initPage(cb) {
     table.appendChild(tr);
   }
   container.appendChild(table);
-  toggleCell(getCell(...INITIAL_HEAD_INDEX));
-  cb();
+  snake.map(cellIndex => fillCell(getCell(...cellIndex)));
 }
